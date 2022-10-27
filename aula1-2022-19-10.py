9a6b63c5f0208fc3cfcd7238cba9dc13b83dcc26
Lucnocx <116230289+Lucnocx@users.noreply.github.com>
2022-10-27 01:05:06 +0000

    update

--- aula1-2022-19-10.py
+++ aula1-2022-19-10.py
@@ -1,12 +1,12 @@
 # Meu primeiro projeto Python!!! 
 #
 # print() = comando de saida
-print("Alo mundo!")
+print("Hello World!")
 
 # Quando quiser guardar uma String! (frase)
-nome = "Henrique Poyatos"
+nome = "Lucas Nocera"
 #Quando quiser guardar um número inteiro
-idade = 42
+idade = 20
 
 #Exibir o meu nome (que está dentro da variável nome)
 print(nome)
